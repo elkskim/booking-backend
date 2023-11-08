90802fea9dd58ef59c449bb7feabb7ad63f70a9c
Mikkel Markers <94105125+elkskim@users.noreply.github.com>
2023-11-08 14:14:48 +0100

    Update Program.cs
    an absolute atrocious way to circumvent CORS

--- booking-backend/Program.cs
+++ booking-backend/Program.cs
@@ -1,9 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using booking_backend.Model;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000/")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowAnyOrigin();
+                      });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ResourceContext>(opt =>
@@ -19,9 +33,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
